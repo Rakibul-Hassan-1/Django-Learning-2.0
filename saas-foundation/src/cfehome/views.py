a75c776a9fdb35905dd7bd970da91d28a30890e2
Rakibul-Hassan-1 <rakibulhassan29052000@gmail.com>
2024-12-13 11:27:24 +0600

    add an new app Visits and db create

--- saas-foundation/src/cfehome/views.py
+++ saas-foundation/src/cfehome/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 import pathlib
 from django.shortcuts import render
-
+from visits.models import PageVisit
 # this_dir = pathlib.Path(__file__).resolve().parent
 def home_page_view(request, *args, **kwargs): 
     # print("This is file path", this_dir)
@@ -10,9 +10,13 @@ def home_page_view(request, *args, **kwargs):
     # html_ = html_file_path.read_text()
     # return HttpResponse(html_)
     # return "hello world" # security issue are happend -> wrong 200 request
+    qs = PageVisit.objects.all()
     name = "Rakib"
     my_title = {
-        "myname" : name
+        "myname" : name,
+        "page_visits_count": qs.count(),
+        "percent": ((qs.count()*100.0)/100)
     }
     html_ = "home.html"
+    PageVisit.objects.create()
     return render(request, html_,my_title)
